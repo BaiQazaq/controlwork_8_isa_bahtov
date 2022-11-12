e9b215e1ae5d88b69914d0b86b53eb426cb0ff9a
Isa Bakhtov <issab79@icloud.com>
2022-11-12 15:45:11 +0600

    Созлана функция регистрации юзера соответственно созданы: шаблон, вьюха, регистрация пути

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 
 from accounts.views.login_view import LoginView
 from accounts.views.logout_view import logout_view
-# from accounts.views.acc_register_view import RegisterView
+from accounts.views.register_view import RegisterView
 # from accounts.views.acc_profile_view import ProfileView
 # from accounts.views.acc_change_view import UserChangeView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', logout_view, name='logout')
+    path('logout/', logout_view, name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
-#  path('register/', RegisterView.as_view(), name='register'),
+
 #     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
 #     path('profile/<int:pk>/change/', UserChangeView.as_view(), name='change'),
